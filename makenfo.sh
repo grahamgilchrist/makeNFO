51f6e2701fd02cb939f47174ab0afa493fd426b5
Graham Gilchrist <mail@grahamgilchrist.com>
2012-10-22 01:46:18 +0100

    Fixed output filename when containing hyphens

--- makenfo.sh
+++ makenfo.sh
@@ -1,16 +1,18 @@
 root=$(pwd)
 movieset=$1
-echo $root
+
 for file in $root/*.mkv
 do
-  filepath="${file%.*}.nfo"
-  filename=$(basename $file)
+  nfofilepath="${file%.*}.nfo"
+  filename=$(basename "$file")
+  filename_noext="${filename%.*}"
+
   echo "Processing $filename"
-  echo "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>\n" > $filepath
-  echo "<movie>" >> $filepath
-  echo "  <title>$filename</title>" >> $filepath
-  echo "  <set>$movieset</set>" >> $filepath
-  echo "</movie>" >> $filepath
-  echo "Output $filepath"
+  echo "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>\n" > "$nfofilepath"
+  echo "<movie>" >> "$nfofilepath"
+  echo "  <title>$filename_noext</title>" >> "$nfofilepath"
+  echo "  <set>$movieset</set>" >> "$nfofilepath"
+  echo "</movie>" >> "$nfofilepath"
+  echo "Output $nfofilepath"
 done
 
